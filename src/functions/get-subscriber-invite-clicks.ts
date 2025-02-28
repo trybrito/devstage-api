d184085f256cff0458ff4e51389e6ef15a632b4f
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-28 01:13:40 -0300

    fix: fixes interface naming

--- src/functions/get-subscriber-invite-clicks.ts
+++ src/functions/get-subscriber-invite-clicks.ts
@@ -1,12 +1,12 @@
 import { redis } from '../redis/client'
 
-interface getSubscriberInviteClicks {
+interface GetSubscriberInviteClicks {
   subscriberId: string
 }
 
 export async function getSubscriberInviteClicks({
   subscriberId,
-}: getSubscriberInviteClicks) {
+}: GetSubscriberInviteClicks) {
   const count = await redis.hget('referral:access-count', subscriberId)
 
   return { count: count ? Number.parseInt(count) : 0 }
